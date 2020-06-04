181ff540a41540e5a4e66bfff2d88f3bec8b6515
Peter Santos <AIMEPTSTSAM@mail.com>
2020-06-04 18:34:23 +0200

    Update CreateDataset.py

--- Python3Code/Chapter2/CreateDataset.py
+++ Python3Code/Chapter2/CreateDataset.py
@@ -39,9 +39,10 @@ class CreateDataset:
     # Add numerical data, we assume timestamps in the form of nanoseconds from the epoch
     def add_numerical_dataset(self, file, timestamp_col, value_cols, aggregation='avg', prefix=''):
         print(f'Reading data from {file}')
-        dataset = pd.read_csv(self.base_dir + file, skipinitialspace=True)
+        dataset = pd.read_csv(self.base_dir / file, skipinitialspace=True)
         # Convert timestamps to dates
-        dataset[timestamp_col] = pd.to_datetime(dataset[timestamp_col], unit='s')
+        dataset[timestamp_col] = pd.to_datetime(dataset[timestamp_col])
+        # dataset[timestamp_col] = pd.to_datetime(dataset[timestamp_col], unit='s')
 
         # Create a table based on the times found in the dataset
         if self.data_table is None:
