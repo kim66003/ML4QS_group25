dabd90ec413a476b1703b37084f3c280b15ad165
Peter Santos <AIMEPTSTSAM@mail.com>
2020-06-05 10:30:34 +0200

    Chapter 3 class creation

--- Python3Code/Assignment_1_3.py
+++ Python3Code/Assignment_1_3.py
@@ -9,10 +9,32 @@ import numpy as np
 from pathlib import Path
 
 data_path = Path('./intermediate_datafiles/')
-data_file = 'chapter3_result_outliers.csv'
-
+data_file = 'chapter2_result.csv'
 
 class OutlierExperiment:
     def __init__(self, data_path, data_file):
-        dataset = pd.read_csv(Path(data_path / data_file), index_col=0)
-        dataset.index = pd.to_datetime(dataset.index)
+        self.dataset = pd.read_csv(Path(data_path / data_file), index_col=0)
+        self.dataset.index = pd.to_datetime(self.dataset.index)
+        self.DataViz = VisualizeDataset(__file__)
+        self.outlier_columns = ['acc_phone_x', 'light_phone_lux']
+        self.OutlierDistr = DistributionBasedOutlierDetection()
+        self.OutlierDist = DistanceBasedOutlierDetection()
+
+    def chauvenet(self, C):
+        for col in self.outlier_columns:
+            print(f"Applying outlier criteria for column {col}")
+            dataset = self.OutlierDistr.chauvenet(self.dataset, col, C)
+            self.DataViz.plot_binary_outliers(dataset, col, col + '_outlier')
+
+    def mixture_model(self, n):
+        for col in self.outlier_columns:
+            print(f"Applying outlier criteria for column {col}")
+            dataset = self.OutlierDistr.mixture_model(self.dataset, col, n)
+            self.DataViz.plot_binary_outliers(dataset, col, col + '_outlier')
+
+    
+
+
+
+experiment = OutlierExperiment(data_path, data_file)
+experiment.chauvenet(4)
--- Python3Code/Chapter3/OutlierDetection.py
+++ Python3Code/Chapter3/OutlierDetection.py
@@ -50,12 +50,12 @@ class DistributionBasedOutlierDetection:
 
     # Fits a mixture model towards the data expressed in col and adds a column with the probability
     # of observing the value given the mixture model.
-    def mixture_model(self, data_table, col):
+    def mixture_model(self, data_table, col, n=3):
 
         print('Applying mixture models')
         # Fit a mixture model to our data.
         data = data_table[data_table[col].notnull()][col]
-        g = GaussianMixture(n_components=3, max_iter=100, n_init=1)
+        g = GaussianMixture(n_components=n, max_iter=100, n_init=1)
         reshaped_data = np.array(data.values.reshape(-1,1))
         g.fit(reshaped_data)
 
