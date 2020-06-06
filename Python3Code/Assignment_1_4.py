5d8cc70cb27971fd1cfb85739b90021878b789cf
kim66003 <kim66003@gmail.com>
2020-06-07 00:38:31 +0200

    ch4 peter version

--- Python3Code/Assignment_1_4.py
+++ Python3Code/Assignment_1_4.py
@@ -14,13 +14,16 @@ DATASET_FNAME = 'chapter3_result_final.csv'
 RESULT_FNAME = 'chapter4_result.csv'
 categorical_abstraction_result_file = 'chapter4_categorical_result.csv'
 
-
 dataset = pd.read_csv(DATA_PATH / DATASET_FNAME, index_col=0)
 dataset.index = pd.Series({x: pd.to_datetime(x) for x in dataset.index})
 DataViz = VisualizeDataset(__file__, show=False)
 milliseconds_per_instance = (dataset.index[1] - dataset.index[0]).microseconds/1000
 
+print('milliseconds per instance', milliseconds_per_instance)
+
 window_sizes = [int(float(5000)/milliseconds_per_instance), int(float(0.5*60000)/milliseconds_per_instance), int(float(5*60000)/milliseconds_per_instance)]
+
+print('window sizes', window_sizes)
 
 NumAbs = NumericalAbstraction()
 CatAbs = CategoricalAbstraction()
@@ -41,6 +44,7 @@ DataViz.plot_dataset(dataset_copy, ['acc_phone_x', 'acc_phone_x_temp_max','acc_p
 dataset_copy.to_csv(DATA_PATH / RESULT_FNAME)
 
 dataset_copy = copy.deepcopy(dataset)
+print('Cat Abs')
 dataset = CatAbs.abstract_categorical(dataset_copy, ['label'], ['like'], 0.03, int(float(5*60000)/milliseconds_per_instance), 2)
 
 dataset_copy.to_csv(DATA_PATH / categorical_abstraction_result_file)
